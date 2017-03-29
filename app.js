00d0279af6b574acaf62ff2f8ac4b5962f1ac544
neysa21 <neysa21@gmail.com>
2017-03-29 17:40:06 -0400

    app js is working

--- app.js
+++ app.js
@@ -1,3 +1,84 @@
+// pry = require('pryjs');
+// var express = require('express');
+// var mongoose = require('mongoose');
+// var path = require('path');
+// var favicon = require('serve-favicon');
+// var logger = require('morgan');
+// var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser');
+// var session = require('express-session');
+// var methodOverride = require('method-override');
+// var db = require('./db');
+// var hbs = require('hbs');
+
+// if (process.env.MONGODB_URI) {
+//   mongoose.connect(process.env.MONGODB_URI)
+// } else {
+//   mongoose.connect('mongodb://localhost/popUps');
+
+
+// //here are all of my requires for my data that I need
+
+// var app = express();
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
+
+// // uncomment after placing your favicon in /public
+// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(methodOverride('_method'));
+// app.use(session({
+//   secret: "derpderpderpcats",
+//   resave: false,
+//   saveUninitialized: false
+// }));
+
+// //here are the things that I need to use so that I can access that data
+
+// var index = require('./routes/index');
+// var users = require('./routes/users');
+// var popUps = require('./routes/popUps');
+// var sessions = require('./routes/sessions');
+
+// //here are my routes that I need to require so that my content can connect to them.
+
+// app.use('/', index);
+// app.use('/users', users);
+// app.use('/popUps', popUps);
+// app.use('/sessions', sessions);
+
+// //I use my routes by calling those variables and putting the path they would go on to find
+// //those files
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+// //if the server goes through all of those routes and can't find something that I listed, 
+// //it will give me the error above
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// module.exports = app;
+
 pry = require('pryjs');
 var express = require('express');
 var mongoose = require('mongoose');
@@ -16,9 +97,10 @@ if (process.env.MONGODB_URI) {
   mongoose.connect(process.env.MONGODB_URI)
 } else {
   mongoose.connect('mongodb://localhost/popUps');
-
+}
 
 //here are all of my requires for my data that I need
+
 
 var app = express();
 
@@ -139,4 +221,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
